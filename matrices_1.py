3757d011e3cac5e0283b67e2a84277c1bb386218
Santiago-tbas <165326212+Santiago-tbas@users.noreply.github.com>
2024-03-28 15:42:31 -0500

    Add files via upload

---
+++ matrices_1.py
@@ -0,0 +1,25 @@
+#PRIMERA FORMA
+
+a=[0]
+
+nc=int(input(print('Inserte numero de columnas:')))
+nf=int(input(print('Ingrese numero de filas')))
+c=[nc*a]
+M=nf*c
+
+print('Matriz de ceros',M)
+
+print(' ')
+print(' ')
+print(' ')
+
+#SEGUNDA FORMA
+
+M2=[]
+
+n2c=int(input(print('Inserte numero de columnas:')))
+n2f=int(input(print('Ingrese numero de filas')))
+
+col=[n2f*a]
+
+M2.append(col)
